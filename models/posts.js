a802fc7db575b4508c6fd2742dec97655bd005bc
Matthew Landen <mlanden1221@gmail.com>
2021-11-22 19:17:55 -0800

    added next route for inifinite scrolling/pagination

--- models/posts.js
+++ models/posts.js
@@ -4,40 +4,58 @@ const db = require('../db');
 const { BadRequestError } = require('../expressError');
 
 class Posts {
-  static async create(post_name, post_data, username) {
-    console.log('Posts.create running');
-    try {
-      const newPost = await db.query(
-        'INSERT INTO posts (name, post_data, username) VALUES ($1, $2, $3) RETURNING *',
-        [post_name, post_data, username]
-      );
-      return { newPost: newPost.rows[0] };
-    } catch (err) {
-      throw new BadRequestError(err);
+    static async create(post_name, post_data, username) {
+        console.log('Posts.create running');
+        try {
+            const newPost = await db.query(
+                'INSERT INTO posts (name, post_data, username) VALUES ($1, $2, $3) RETURNING *',
+                [post_name, post_data, username]
+            );
+            return { newPost: newPost.rows[0] };
+        } catch (err) {
+            throw new BadRequestError(err);
+        }
     }
-  }
 
-  static async getPost(id) {
-    console.log('Posts.getPost running');
-    try {
-      const post = await db.query(`SELECT * FROM posts WHERE id= $1;`, [id]);
-      return { post: post.rows[0] };
-    } catch (err) {
-      throw new BadRequestError(err);
+    static async getPost(id) {
+        console.log('Posts.getPost running');
+        try {
+            const post = await db.query(`SELECT * FROM posts WHERE id= $1;`, [
+                id,
+            ]);
+            return { post: post.rows[0] };
+        } catch (err) {
+            throw new BadRequestError(err);
+        }
     }
-  }
 
-  static async getMostRecent(numOfPosts) {
-    console.log('getMostRecent running');
-    try {
-      const mostRecent = await db.query(
-        `SELECT * FROM posts ORDER BY id DESC Limit ${numOfPosts};`
-      );
-      return { posts: mostRecent.rows };
-    } catch (err) {
-      throw new BadRequestError(`No posts found`);
+    static async getMostRecent(numOfPosts) {
+        console.log('getMostRecent running');
+        try {
+            const mostRecent = await db.query(
+                `SELECT * FROM posts ORDER BY id DESC Limit $1;`,
+                [numOfPosts]
+            );
+            return { posts: mostRecent.rows };
+        } catch (err) {
+            throw new BadRequestError(`No posts found`);
+        }
     }
-  }
+
+    // Get ID of most recent post SELECT id FROM posts ORDER BY id DESC LIMIT 1;
+    // Return id, post_data, username
+    static async nextPosts(numOfPosts, currLastId) {
+        console.log('nextPost running');
+        try {
+            const nextPost = await db.query(
+                `SELECT id, post_data, username FROM posts WHERE id < $1 ORDER BY id DESC Limit $2;`,
+                [currLastId, numOfPosts]
+            );
+            return { posts: nextPost.rows };
+        } catch (err) {
+            throw new BadRequestError(`No posts found`);
+        }
+    }
 }
 
 module.exports = Posts;
--- routes/posts.js
+++ routes/posts.js
@@ -9,35 +9,55 @@ const router = express.Router({ mergeParams: true });
 
 // returns {newPost: {postDetails}}
 router.post('/new', async (req, res, next) => {
-  try {
-    const { post_name, post_data, username } = req.body;
-    const newPost = await Posts.create(post_name, post_data, username);
-    return res.status(201).json(newPost);
-  } catch (err) {
-    next(err);
-  }
+    try {
+        const { post_name, post_data, username } = req.body;
+        const newPost = await Posts.create(post_name, post_data, username);
+        return res.status(201).json(newPost);
+    } catch (err) {
+        next(err);
+    }
 });
 
 // returns {post: {postDetails}}
 router.get('/id/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const post = await Posts.getPost(id);
-    return res.status(201).json(post);
-  } catch (err) {
-    next(err);
-  }
+    try {
+        const { id } = req.params;
+        const post = await Posts.getPost(id);
+        return res.status(201).json(post);
+    } catch (err) {
+        next(err);
+    }
 });
 
+// returns {posts: [{postDetails}]}
 router.get('/count/:numPosts', async (req, res) => {
-  const numPosts = parseInt(req.params.numPosts);
-  console.log(typeof numPosts);
-  if (typeof numPosts !== 'number') {
-    throw new BadRequestError('invalid number input');
-  }
+    try {
+        const numPosts = parseInt(req.params.numPosts);
+        console.log(typeof numPosts);
+        if (typeof numPosts !== 'number') {
+            throw new BadRequestError('invalid number input');
+        }
 
-  const mostRecentPosts = await Posts.getMostRecent(numPosts);
-  return res.status(201).json(mostRecentPosts);
+        const mostRecentPosts = await Posts.getMostRecent(numPosts);
+        return res.status(201).json(mostRecentPosts);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// Route used for infinite scroll
+router.get('/next/:numPosts/:currLastId', async (req, res, next) => {
+    try {
+        const numPosts = parseInt(req.params.numPosts);
+        const currLastId = parseInt(req.params.currLastId);
+        if (typeof numPosts !== 'number' || typeof currLastId !== 'number') {
+            throw new BadRequestError('invalid number input');
+        }
+        const nextPostsBatch = await Posts.nextPosts(numPosts, currLastId);
+        return res.status(201).json(nextPostsBatch);
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
